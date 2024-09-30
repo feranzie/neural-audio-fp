09ce9bbe4b07164dbe5d5fc76dbcbdb304faee5e
feranzie <oladaviesferanmi@gmail.com>
2024-09-30 07:32:08 -0700

    audi args

--- split_classification.py
+++ split_classification.py
@@ -3,31 +3,42 @@ from pydub import AudioSegment
 import os
 from datetime import timedelta
 
+import argparse
+import re
+
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser(description='Split all audio files in a folder into segments.')
+
+# Add arguments for input directory and output directory
+parser.add_argument('--input', type=str, required=True, help='Path to the input csv ')
+parser.add_argument('--output_dir', type=str, required=True, help='Path to the output directory where classification output would be saved.')
+parser.add_argument('--audio', type=str, required=True, help='Path to the audio file.')
+
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Set the input and output directories from the parsed arguments
+input_csv = args.input
+output_dir = args.output_dir
+audio_file_path = args.audio
+
 # Load the CSV file
-df = pd.read_csv('s-CZWGsZK4_2024-09-24_17-00-03_results_mark.csv')
+df = pd.read_csv(input_csv)
 
 # Convert 'start' and 'end' columns to timedelta
 df['start'] = pd.to_timedelta(df['start'])
 df['end'] = pd.to_timedelta(df['end'])
 
-# df['start_seconds'] = pd.to_timedelta(df['start']).dt.total_seconds()
-# df['end_seconds'] = pd.to_timedelta(df['end']).dt.total_seconds()
 
-# # cc=[]
-# # for i in range(len(df)):
-# #     bb=df.iloc[i]["path"] +"_"+ str(df.iloc[i]["start_seconds"])+ "_" +str(df.iloc[i]["end_seconds"])
-# #     cc.append(bb)
-# # df["combined"]=cc
-
-# Path to the original audio file
-audio_file_path = 's-CZWGsZK4_2024-09-24_17-00-03.mp3'  # Replace with your actual audio file path
+#audio_file_path = 's-CZWGsZK4_2024-09-24_17-00-03.mp3'  # Replace with your actual audio file path
 
 # Load the audio file
 audio = AudioSegment.from_file(audio_file_path)
 
 # Create output directories if they do not exist
-output_dir_music = 'bmusic_segments'
-output_dir_no_music = 'bno_music_segments'
+output_dir_music = f'{output_dir}/music_segments'
+output_dir_no_music = f'{output_dir}/no_music_segments'
 
 os.makedirs(output_dir_music, exist_ok=True)
 os.makedirs(output_dir_no_music, exist_ok=True)
